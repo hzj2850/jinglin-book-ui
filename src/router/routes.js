a741317e3653665de895af56b48aad809493169e
hzj2850 <48989681+hzj2850@users.noreply.github.com>
2024-07-28 20:38:11 +0800

    路由优化

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,58 +11,64 @@ const routes = [
         redirect: '/book/makeList',
         children: [
             {
-                path: 'makeList',
+                path: 'bookMake/list',
                 name: 'makeList',
                 component: () => import(/* webpackChunkName: "makeList" */ '../views/bookMake/list.vue'),
                 meta: { title: '文书制作', keepAlive: false }
             },
             {
-                path: 'makeDetails',
+                path: 'bookMake/details',
                 name: 'makeDetails',
                 component: () => import(/* webpackChunkName: "makeDetails" */ '../views/bookMake/details.vue'),
                 meta: { title: '文书制作', keepAlive: true }
             },
             {
-                path: 'exaList',
+                path: 'bookExa/list',
                 name: 'exaList',
                 component: () => import(/* webpackChunkName: "makeList" */ '../views/bookExa/list.vue'),
                 meta: { title: '文书审核', keepAlive: false }
             },
             {
-                path: 'exaDetails',
+                path: 'bookExa/details',
                 name: 'exaDetails',
                 component: () => import(/* webpackChunkName: "exaDetails" */ '../views/bookExa/details.vue'),
                 meta: { title: '文书审核', keepAlive: true }
             },
             {
-                path: 'appList',
+                path: 'bookApp/list',
                 name: 'appList',
                 component: () => import(/* webpackChunkName: "appList" */ '../views/bookApp/list.vue'),
                 meta: { title: '文书审批', keepAlive: false }
             },
             {
-                path: 'appDetails',
+                path: 'bookApp/details',
                 name: 'appDetails',
                 component: () => import(/* webpackChunkName: "appDetails" */ '../views/bookApp/details.vue'),
                 meta: { title: '文书审批', keepAlive: true }
             },
             {
-                path: 'IssList',
+                path: 'bookIss/list',
                 name: 'IssList',
                 component: () => import(/* webpackChunkName: "IssList" */ '../views/bookIss/list.vue'),
                 meta: { title: '文书签发', keepAlive: false }
             },
             {
-                path: 'IssDetails',
+                path: 'bookIss/details',
                 name: 'IssDetails',
                 component: () => import(/* webpackChunkName: "IssDetails" */ '../views/bookIss/details.vue'),
                 meta: { title: '文书签发', keepAlive: true }
             },
             {
-                path: 'bookPrint',
-                name: 'bookPrint',
-                component: () => import(/* webpackChunkName: "bookPrint" */ '../views/bookPrint/list.vue'),
-                meta: { title: '表单验证', keepAlive: false }
+                path: 'bookPrint/list',
+                name: 'bookPrintList',
+                component: () => import(/* webpackChunkName: "bookPrintList" */ '../views/bookPrint/list.vue'),
+                meta: { title: '文书打印', keepAlive: false }
+            },
+            {
+                path: 'bookPrint/details',
+                name: 'bookPrintDetails',
+                component: () => import(/* webpackChunkName: "bookPrintDetails" */ '../views/bookPrint/details.vue'),
+                meta: { title: '文书打印', keepAlive: true }
             },
         ],
     }
---
+++ src/views/bookPrint/details.vue
@@ -0,0 +1,15 @@
+<template>
+    <caseDetails
+        title="文书待打印任务"
+        listPath="bookPrintList"
+    />
+</template>
+
+<script>
+import caseDetails from '../bookcom/details/index.vue';
+export default {
+    components: {
+        caseDetails
+    },
+}
+</script>
