4ceb44891edf65a5e7eadf07c7d00ffea97ce415
hzj2850 <48989681+hzj2850@users.noreply.github.com>
2024-07-29 10:12:18 +0800

    表格优化

--- src/components/ant-table/tree.js
+++ src/components/ant-table/tree.js
@@ -5,7 +5,7 @@ export const ckIcon = (h, c, e, f) => {
     const keys = c.props.selectRowKeys || [];
     const key = c.props.rowKey;
     const idx = keys.indexOf(e[key]);
-    const icon = idx == -1 ? 'icon-check1' : 'icon-check2';
+    const icon = idx == -1 ? 'icon-check1' : 'icon-check3';
     return h('i', {
         class: ['iconfont', 'iconcheck', icon],
         on: {
